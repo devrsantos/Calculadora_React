d0456cf78683285f31dcdcf4f203fdb575d27a2e
Renan Augusto dos Santos <devrsantos@gmail.com>
2023-01-26 11:18:05 -0300

    Designer Concluído

--- src/components/DisplayCalc.tsx
+++ src/components/DisplayCalc.tsx
@@ -1,5 +1,10 @@
 import styles from "./DisplayCalc.module.css";
 
+import timeMachineLogo from "./../assets/Time Machine.svg";
+import rulerLogo from "./../assets/Ruler.svg";
+import rootLogo from "./../assets/Root.svg";
+import backspaceLogo from "./../assets/Backspace.svg";
+
 const DisplayCalc = () => {
   return (
     <div className={styles.container}>
@@ -8,6 +13,18 @@ const DisplayCalc = () => {
         </div>
         <div className={styles.result}>
             <p>9</p>
+        </div>
+        <div className={styles.icons}>
+          <div className={styles.iconRoles}>
+            <span><img src={timeMachineLogo} /></span>
+            <span><img src={rulerLogo} /></span>
+            <span><img src={rootLogo} /></span>
+          </div>
+          <div className={styles.iconsBackspace}>
+            <span>
+              <img src={backspaceLogo} />
+            </span>
+          </div>
         </div>
     </div>
   )
