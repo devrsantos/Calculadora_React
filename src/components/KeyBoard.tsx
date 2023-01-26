c2370daa5be6a202d93a07d55a86d65cba4ab5a0
Renan Augusto dos Santos <devrsantos@gmail.com>
2023-01-26 11:46:48 -0300

    Obtendo os Clicks

--- src/components/KeyBoard.tsx
+++ src/components/KeyBoard.tsx
@@ -9,37 +9,70 @@ import subtrationLogo from "./../assets/Subtraction.svg";
 import additionLogo from "./../assets/Addition.svg";
 
 const KeyBoard = () => {
+
+  function alertClick() {
+    console.log(event?.target.textContent);
+  }
+
+  function handleClickClear() {
+    console.log("Clear clicked");
+  }
+
+  function handleClickPorcent() {
+    console.log("Porcent clicked");
+  }
+
+  function handleClickDivision() {
+    console.log("Division clicked");
+  }
+
+  function handleClickMultiplication() {
+    console.log("Multiplication clicked");
+  }
+
+  function handleClickSubtraction() {
+    console.log("Subtraction clicked");
+  }
+
+  function handleClickAddition() {
+    console.log("Addition clicked");
+  }
+
+  function handleClickAmount() {
+    console.log("Amount clicked");
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.line}>
-        <Button onClick={""} value={"C"} clear></Button>
-        <Button onClick={""} value={<img src={parentsLogo} />}></Button>
-        <Button onClick={""} value={<img src={porcentLogo} />}></Button>
-        <Button onClick={""} value={<img src={divisionLogo} />}></Button>
+        <Button onClick={handleClickClear} value={"C"} clear>C</Button>
+        <Button onClick={""} value={<img src={parentsLogo} />}>()</Button>
+        <Button onClick={handleClickPorcent} value={<img src={porcentLogo} />}></Button>
+        <Button onClick={handleClickDivision} value={<img src={divisionLogo} />}></Button>
       </div>
       <div className={styles.line}>
-        <Button onClick={""} value={"7"}></Button>
-        <Button onClick={""} value={"8"}></Button>
-        <Button onClick={""} value={"9"}></Button>
-        <Button onClick={""} value={<img src={multiplicationLogo} />}></Button>
+        <Button onClick={alertClick} value={"7"}></Button>
+        <Button onClick={alertClick} value={"8"}></Button>
+        <Button onClick={alertClick} value={"9"}></Button>
+        <Button onClick={handleClickMultiplication} value={<img src={multiplicationLogo} />}></Button>
       </div>
       <div className={styles.line}>
-        <Button onClick={""} value={"4"}></Button>
-        <Button onClick={""} value={"5"}></Button>
-        <Button onClick={""} value={"6"}></Button>
-        <Button onClick={""} value={<img src={subtrationLogo} />}></Button>
+        <Button onClick={alertClick} value={"4"}></Button>
+        <Button onClick={alertClick} value={"5"}></Button>
+        <Button onClick={alertClick} value={"6"}></Button>
+        <Button onClick={handleClickSubtraction} value={<img src={subtrationLogo} />}></Button>
       </div>
       <div className={styles.line}>
-        <Button onClick={""} value={"1"}></Button>
-        <Button onClick={""} value={"2"}></Button>
-        <Button onClick={""} value={"3"}></Button>
-        <Button onClick={""} value={<img src={additionLogo} />}></Button>
+        <Button onClick={alertClick} value={"1"}></Button>
+        <Button onClick={alertClick} value={"2"}></Button>
+        <Button onClick={alertClick} value={"3"}></Button>
+        <Button onClick={handleClickAddition} value={<img src={additionLogo} />}></Button>
       </div>
       <div className={styles.line}>
-        <Button onClick={""} value={"+/-"}></Button>
-        <Button onClick={""} value={"0"}></Button>
-        <Button onClick={""} value={"."}></Button>
-        <Button onClick={""} value={"="} total></Button>
+        <Button onClick={alertClick} value={"+/-"}></Button>
+        <Button onClick={alertClick} value={"0"}></Button>
+        <Button onClick={alertClick} value={"."}></Button>
+        <Button onClick={handleClickAmount} value={"="} total></Button>
       </div>
     </div>
   );
